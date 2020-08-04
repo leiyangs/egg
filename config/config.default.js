36189133575422fcba059294aea3bdf7c606b03a
yanglei <yanglei@shjinjia.com>
2020-08-04 16:30:59 +0800

    渲染html

--- config/config.default.js
+++ config/config.default.js
@@ -1,16 +1,20 @@
 // keys用来加密cookie 服务器把cookie发送给客户端，为了防止客户篡改数据，需要设置一个密码
 // exports.keys = 'yy';
 
+let path = require('path');
 // 如果需要使用插件的话用下面的方式
-module.exports = app =>{
+module.exports = app => {
   let config = {};
-  config.keys = app.name+new Date(); // 配置加密key
-  config.view = { //配置视图
+  config.keys = 'yy'; // 配置加密key
+  config.view = { // 配置视图
     defaultExtension: '.html', // 默认扩展名：如果渲染一个文件的扩展名没有指定并且找不到这个文件，就会默认扩展名为.html再次寻找
     defaultViewEngine: 'nunjucks', // 默认渲染：如果摸个文件的扩展名没有在mapping中配置，默认nunjucks处理
     mapping: {
-      '.html': 'nunjucks'
-    }
+      '.html':'nunjucks'
+    },
+    root: [ // 渲染文件 设置根路径
+      path.join(app.baseDir, 'app/views')
+    ].join(',')
   }
   return config;
 }
